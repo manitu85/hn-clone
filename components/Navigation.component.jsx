63dc2e8396ebf91c54f0191189f727209655816d
manitu85 <manitu.psy@gmail.com>
2020-06-24 00:17:50 +0200

    #

--- components/Navigation.component.jsx
+++ components/Navigation.component.jsx
@@ -34,3 +34,12 @@ const Navigation = () => (
 
 
 export default Navigation
+
+// Examples
+// const links = [
+//   { href: "https://zeit.co/now", label: "ZEIT" },
+//   { href: "https://github.com/zeit/next.js", label: "GitHub" }
+// ].map(link => {
+//   link.key = `nav-link-${link.href}-${link.label}`;
+//   return link;
+// });
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import Head from 'next/head'
 import PropTypes from 'prop-types'
+import Router from 'next/router'
+import NProgress from 'nprogress'
 
 import { ThemeProvider as StyledThemeProvider } from 'styled-components'
 import GlobalStyle from '@/styles/GlobalStyles'
 import { theme } from '@/styles/theme'
 import '@/styles/tailwind.css'
+import 'nprogress/nprogress.css'
 
+//Binding events
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 const App = props => {
   const { Component, pageProps } = props
