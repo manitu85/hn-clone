14d39b10668d4eef0d3401d5a070e4d3b8f960bf
manitu85 <manitu.psy@gmail.com>
2020-06-24 02:11:00 +0200

    #

--- postcss.config.js
+++ postcss.config.js
@@ -1,36 +1,36 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-    ...(process.env.NODE_ENV === 'production'
-      ? {
-        '@fullhuman/postcss-purgecss': {
-          content: [
-            './components/**/*.js',
-            './pages/**/*.js'
-          ],
-          defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-        }
-      }
-      : {})
-  }
-};
-
 // module.exports = {
-//   plugins: [
-//     'tailwindcss',
-//     'postcss-flexbugs-fixes',
-//     [
-//       'postcss-preset-env',
-//       {
-//         autoprefixer: {
-//           flexbox: 'no-2009',
-//         },
-//         stage: 3,
-//         features: {
-//           'custom-properties': false,
-//         },
-//       },
-//     ],
-//   ],
-// }
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//     ...(process.env.NODE_ENV === 'production'
+//       ? {
+//         '@fullhuman/postcss-purgecss': {
+//           content: [
+//             './components/**/*.jsx',
+//             './pages/**/*.js'
+//           ],
+//           defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+//         }
+//       }
+//       : {})
+//   }
+// };
+
+module.exports = {
+  plugins: [
+    'tailwindcss',
+    'postcss-flexbugs-fixes',
+    [
+      'postcss-preset-env',
+      {
+        autoprefixer: {
+          flexbox: 'no-2009',
+        },
+        stage: 3,
+        features: {
+          'custom-properties': false,
+        },
+      },
+    ],
+  ],
+}
