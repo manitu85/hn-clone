3c5f3498e841fd93453033352fbf07a962256be1
manitu85 <manitu.psy@gmail.com>
2020-07-06 18:38:52 +0200

    #

--- components/Pagination.component.jsx
+++ components/Pagination.component.jsx
@@ -2,8 +2,8 @@ import Link from 'next/link'
 
 const Pagination = ({ page }) => (
   <div className="flex justify-start ml-6 mb-5 my-5">
-    <Link href={`/?page=${page - 1}`} >
-      <button className="bg-white hover:bg-gray-100 text-gray-600  font-semibold text-sm py-1 px-5 border border-gray-400 rounded shadow">Prev</button>
+    <Link href={`/?page=${page - 1}`}>
+      <button disabled={page <= 0} className="bg-white hover:bg-gray-100 text-gray-600  font-semibold text-sm py-1 px-5 border border-gray-400 rounded shadow">Prev</button>
     </Link>
     <Link href={`/?page=${page + 1}`}>
       <button className="bg-white hover:bg-gray-100 text-gray-600 font-semibold text-sm py-1 px-5 border border-gray-400 rounded shadow">Next</button>
