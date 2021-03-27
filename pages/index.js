01cb40c013dace2c4c9632729b8e29af1ee8f8ba
manitu85 <manitu.psy@gmail.com>
2021-03-28 00:49:00 +0100

    #

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
-import Error from "next/error";
-import axios from "axios";
 import useSWR from "swr";
-import { HN_API } from "@/lib/hnApi";
+import Error from "next/error";
 
 import StoryListComponent from "@/components/StoryList.component";
 import Layout from "hoc/Layout.component";
