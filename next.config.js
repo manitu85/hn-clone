b353f9fc28e13076195618cf1de5156ce60d3f60
manitu85 <manitu.psy@gmail.com>
2021-03-28 21:38:40 +0200

    #

--- next.config.js
+++ next.config.js
@@ -1,33 +1,37 @@
-const withOffline = require("next-offline");
+// const withOffline = require("next-offline");
 
-const nextConfig = {
-  workboxOpts: {
-    swDest: process.env.NEXT_EXPORT
-      ? "service-worker.js"
-      : "static/service-worker.js",
-    runtimeCaching: [
-      {
-        urlPattern: /^https?.*/,
-        handler: "NetworkFirst",
-        options: {
-          cacheName: "offlineCache",
-          expiration: {
-            maxEntries: 200,
-          },
-        },
-      },
-    ],
-  },
-  experimental: {
-    async rewrites() {
-      return [
-        {
-          source: "/service-worker.js",
-          destination: "/_next/static/service-worker.js",
-        },
-      ];
-    },
-  },
+// const nextConfig = {
+//   workboxOpts: {
+//     swDest: process.env.NEXT_EXPORT
+//       ? "service-worker.js"
+//       : "static/service-worker.js",
+//     runtimeCaching: [
+//       {
+//         urlPattern: /^https?.*/,
+//         handler: "NetworkFirst",
+//         options: {
+//           cacheName: "offlineCache",
+//           expiration: {
+//             maxEntries: 200,
+//           },
+//         },
+//       },
+//     ],
+//   },
+//   experimental: {
+//     async rewrites() {
+//       return [
+//         {
+//           source: "/service-worker.js",
+//           destination: "/_next/static/service-worker.js",
+//         },
+//       ];
+//     },
+//   },
+// };
+
+// module.exports = withOffline(nextConfig);
+
+module.exports = {
+  target: "serverless",
 };
-
-module.exports = withOffline(nextConfig);
